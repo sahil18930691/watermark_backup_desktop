5ecc56f0d35d4f01fef323174577a895569e0769
unknown <kshitij.das@squareyards.co.in>
2021-10-06 13:04:08 +0530

    changed expose in dockerfile

--- api/main.py
+++ api/main.py
@@ -100,7 +100,8 @@ def total_req_logo_enhancement(k):
 
 load_dotenv()
 
-connection=os.environ['MONGODB_URI']
+#connection=os.environ['MONGODB_URI']
+connection=os.getenv('MONGODB_URI')
 
 cluster=MongoClient(connection, tlsCAFile=ca)
 db = cluster["watermark_database"]
